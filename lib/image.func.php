0f008575760bf108384e721efb2036ec1cf7d029
lshunran <lshunran@163.com>
2016-01-12 21:39:19 +0800

    verify function

--- lib/image.func.php
+++ lib/image.func.php
@@ -4,6 +4,7 @@ require_once 'string.func.php';
 function verifyImage($type=1,$length=4,$pixel=0,$line=0,$sess_name = "verify"){
 	session_start();
 	//创建画布
+	
 	$width = 80;
 	$height = 28;
 	$image = imagecreatetruecolor ( $width, $height );
@@ -21,7 +22,7 @@ function verifyImage($type=1,$length=4,$pixel=0,$line=0,$sess_name = "verify"){
 		$angle = mt_rand ( - 15, 15 );
 		$x = 5 + $i * $size;
 		$y = mt_rand ( 20, 26 );
-		$fontfile = "../fonts/" . $fontfiles [mt_rand ( 0, count ( $fontfiles ) - 1 )];
+		$fontfile = "../fonts/" . $fontfiles [0];
 		$color = imagecolorallocate ( $image, mt_rand ( 50, 90 ), mt_rand ( 80, 200 ), mt_rand ( 90, 180 ) );
 		$text = substr ( $chars, $i, 1 );
 		imagettftext ( $image, $size, $angle, $x, $y, $color, $fontfile, $text );
@@ -37,6 +38,7 @@ function verifyImage($type=1,$length=4,$pixel=0,$line=0,$sess_name = "verify"){
 			imageline ( $image, mt_rand ( 0, $width - 1 ), mt_rand ( 0, $height - 1 ), mt_rand ( 0, $width - 1 ), mt_rand ( 0, $height - 1 ), $color );
 		}
 	}
+	ob_clean();
 	header ( "content-type:image/gif" );
 	imagegif ( $image );
 	imagedestroy ( $image );
