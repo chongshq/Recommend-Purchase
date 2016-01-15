982b3dedbf1bf0898542fb33b0ea4042a36713ca
qian.cs <qian.cs@outlook.com>
2016-01-15 18:31:04 +0800

    no message

--- index.php
+++ index.php
@@ -211,7 +211,7 @@ if(!($cates && is_array($cates))){
 <div class="hr_25"></div>
 <div class="footer">
 	<p><a href="#">简介</a><i>|</i><a href="#">公告</a><i>|</i> <a href="#">招纳贤士</a><i>|</i><a href="#">联系我们</a><i>|</i>客服热线：400-675-1234</p>
-	<p>Copyright &copy; 2006 - 2014 慕课版权所有&nbsp;&nbsp;&nbsp;京ICP备09037834号&nbsp;&nbsp;&nbsp;京ICP证B1034-8373号&nbsp;&nbsp;&nbsp;某市公安局XX分局备案编号：123456789123</p>
+	<p>Copyright &copy; 2006 - 2014 版权所有&nbsp;&nbsp;&nbsp;京ICP备09037834号&nbsp;&nbsp;&nbsp;京ICP证B1034-8373号&nbsp;&nbsp;&nbsp;某市公安局XX分局备案编号：123456789123</p>
 	<p class="web"><a href="#"><img src="images/webLogo.jpg" alt="logo"></a></p>
 </div>
 </body>
